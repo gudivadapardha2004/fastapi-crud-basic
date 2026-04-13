1c992676545d945681f4180addfd9972b48821b9
Gudivada Pardha Srikanth <gudivadapardha5@gmail.com>
2026-04-13 13:25:29 +0530

    Change database URL to placeholder
    Updated database connection URL placeholder.

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import sessionmaker,declarative_base
 from sqlalchemy import create_engine
 
-db_url = "mysql+pymysql://root:pardha%40123@localhost:3306/users"
+db_url = "YOURS DB URL ID"
 engine=create_engine(db_url)
 session=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
